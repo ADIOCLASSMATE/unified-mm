e493d054ea1de239f45af00c35fdb75c5814104d
wjx <cqzxwjx@163.com>
2026-09-02 16:33:59 +0000

    Revert "clone prepared NPU attention masks per call"
    This reverts commit 9e4e20032ac3e35c2db6bf8d55344c68ec242427.

--- models/modeling_model/modeling_selfless_flow.py
+++ models/modeling_model/modeling_selfless_flow.py
@@ -414,7 +414,6 @@ def compiled_flex_attention(query, key, value, attention_mask, scaling, enable_g
                 "prepared attention_mask must contain (safe_mask, valid_rows)"
             )
         safe_mask, valid_rows = attention_mask
-        safe_mask = safe_mask.clone()
     else:
         safe_mask, valid_rows = _to_bool_atten_mask(attention_mask)
     out = torch_npu.npu_fusion_attention(
